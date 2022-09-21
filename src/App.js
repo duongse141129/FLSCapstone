d1f0b78e1c8ebcb3f1ce5f7e340e1ab208cd6bf2
ocean-p <duongkodoiqua@gmail.com>
2022-09-21 22:10:46 +0700

    change title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          FLS - FPT Lecturer Scheduler
+          FLS - FPT Lecturer Scheduler 2022
         </p>
         <a
           className="App-link"
