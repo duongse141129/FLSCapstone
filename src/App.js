c9a0d9d8f842fd0915f5802dbb82d0041c31093b
ocean-p <duongkodoiqua@gmail.com>
2022-12-08 14:08:54 +0700

    update import components into file App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import Profile from "./components/profile/Profile";
-import Main from "./components/main/Main";
-import LecturerPage from "./pages/LecturerPage";
-import Login from "./pages/Login";
-import Semester from "./components/semester/Semester";
-import Department from "./components/department/Department";
-import ManagerPage from "./pages/ManagerPage";
-import Lecturer from './components/lecturer/Lecturer';
-import LecturerInfo from "./components/lecturer/LecturerInfo";
-import SubjectOfManager from "./components/subject/SubjectOfManager";
-import ScheduleManager from "./components/schedule/ScheduleManager";
-import ScheduleDetail from "./components/schedule/ScheduleDetail";
-import SemesterDetail from './components/semester/SemesterDetail'
-import ManagerHome from "./components/home/ManagerHome";
-import SemesterDetailManager from "./components/semester/manager/SemesterDetailManager";
-import AdminPage from "./pages/AdminPage";
-import LecturerAdmin from "./components/lecturer/LecturerAdmin";
-import LecturerInfoAdmin from "./components/lecturer/LecturerInfoAdmin";
-import DepartmentAdmin from "./components/department/DepartmentAdmin";
-import SubjectAdmin from "./components/subject/SubjectAdmin";
-import Slot from "./components/slot/Slot";
-import SemesterAdmin from "./components/semester/admin/SemesterAdmin";
-import SemesterDetailAdmin from "./components/semester/admin/SemesterDetailAdmin";
-import DepartmentManager from "./components/department/DepartmentManager";
+import {AdminPage, LecturerPage, ManagerPage, Login} from './pages';
+import {Main, Profile, Semester, SemesterDetail, DepartmentManager, 
+  Department, ManagerHome, Lecturer, LecturerInfo, SubjectOfManager, 
+  ScheduleManager, ScheduleDetail, SemesterDetailManager, LecturerAdmin, 
+  LecturerInfoAdmin, DepartmentAdmin, SubjectAdmin, Slot, SemesterAdmin,
+  SemesterDetailAdmin} from './components';
 
 function App() {
   return (
